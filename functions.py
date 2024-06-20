660f62a190d2a987f46be7423e0f23555979a5f2
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-06-20 13:20:38 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+
+import math
+#This code is calculating distance between two coordinate information
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # Earth radius in kilometers
+
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+
+    return distance
